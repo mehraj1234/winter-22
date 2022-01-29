ebcd48566419d535352f702cbf6d866769c86a23
mehraj1234 <97043391+mehraj1234@users.noreply.github.com>
2022-01-29 01:05:32 -0500

    Add files via upload

--- -63899979315635066131a_exercises.sql
+++ -63899979315635066131a_exercises.sql
@@ -31,25 +31,25 @@
 --------------------------------------------------------
 
 -- 1. Write a SQL query to view the entire 'users' table.
-      
-    SELECT * FROM users;
+
+
 -- 2. View the first 5 rows of the 'users' table. 
-       SELECT * FROM users LIMIT 5;
+
 
 -- 3. View the 'users' table ordered by when the users was created.
-      SELECT * FROM users ORDER BY created_at;
+
 
 -- 4. Write a SQL query to view the entire 'purchases' table.
-      SELECT * FROM purchases;
+
 
 -- 5. View the name and state columns of the 'purchases' table.
-       SELECT  name, state FROM purchases;
+
 
 -- 6. Use LIMIT and ORDER BY to see the 10 most recent purchases. 
-      SELECT * FROM purchases ORDER BY created_at DESC LIMIT 10;
+
 
 -- 7. Use a DISTINCT query to see which states people have made purchases from. 
-       SELECT DISTINCT state FROM purchases;
+
 
 ----------------------------------------
 -- EXTRA CREDIT: If you finish early. --
@@ -57,8 +57,7 @@
 
 -- 1. You can use DISTINCT on multiple columns as well.
 --    Use DISTINCT on the purchases table to see which states each user has made orders from.
-      SELECT * FROM purchases;
-      SELECT DISTINCT user_id, state FROM purchases;
+
 
 -- 2. Modify the query above to ORDER BY user_id, so you can see the data user-by-user.
-        SELECT DISTINCT user_id, state FROM purchases ORDER BY user_id;
+
