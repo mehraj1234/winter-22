4dd306991c4735d6bcb0d8011b8c98bc38893010
mehraj1234 <97043391+mehraj1234@users.noreply.github.com>
2022-01-29 01:10:45 -0500

    Add files via upload

---
+++ 66840628134535944292b_exercises.sql
@@ -0,0 +1,43 @@
+
+-------------------------------------------------------------------
+-- WARM UPS: Type the following commands to build muscle memory. --
+-------------------------------------------------------------------
+
+-- 1. [Using max/min]: SELECT max(price) FROM products;
+
+-- 2. [Using count]: SELECT count(*) FROM users;
+
+-- 3. [Using sum]: SELECT sum(price) FROM purchase_items;
+
+-- 4. [Using avg]: SELECT avg(price) FROM purchase_items;
+
+--------------------------------------------------------
+-- EXERCISES: Answer using the techniques from above. --
+--------------------------------------------------------
+
+-- 1. Find the total number of purchases.
+
+-- 2. Find the average price of the items in the products table.
+
+-- 3. Find the maximum price from the products that are NOT a
+--    computer.
+
+-- 4. Find the number of users with Gmail email addresses.
+
+-- 5. Using the purchase_items table, find the total dollar value of
+--    all items with state "Returned".
+
+-- 6. Find the average price of all products containing the word
+--    "Book" in their title.
+
+----------------------------------------
+-- EXTRA CREDIT: If you finish early. --
+----------------------------------------
+
+-- 1. You use multiple aggregation functions in one SELECT. Using
+--    only one query,
+--    find the min(), max() and avg() of the prices in the product
+--    table.
+
+-- 2. In one query, find the difference between the price of the most
+--    expensive and least expensive product.
